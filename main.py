d8015089ca4ae727fb9d4a81d82b67f80fd324b4
Raul <raulrufino0@gmail.com>
2022-06-24 17:56:02 -0300

    Cria modulo para renderizar caminho

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 
-from src.cities import Cities
+from uuid import uuid1
+
 from src.tsp_file_reader import reader
+from src.subida_encosta import busca
+from src.renderiza_caminho import desenha_solucao
+
 
 if __name__ == '__main__':
     path = 'data-samples/wi29.tsp'
-    _, cities = reader(path)
-    print(cities.obtem_distancia(0, 1))
+
+    c1 = [(0, 0), (0, 1), (1, 1)]
+
+    _, caminho = reader(path)
+
+    solution = busca(caminho)
+
+    id = str(uuid1())
+
+    desenha_solucao(caminho, id, f'result.png')
---
+++ src/renderiza_caminho.py
@@ -0,0 +1,27 @@
+
+import matplotlib.pyplot as plt
+from matplotlib.path import Path
+import matplotlib.patches as patches
+
+from src.cities import CaminhoDoCaixeiro, separa_cordenadas, obtem_custo
+
+def desenha_pontos(ax, solucao):
+    xs, ys = separa_cordenadas(solucao)
+    ax.scatter(xs, ys, c='blue')
+
+def desenha_caminho(ax, solucao):
+    codigos = [Path.MOVETO] + (len(solucao) * [Path.LINETO])
+    path = Path(solucao + [solucao[0]], codigos)
+    patch = patches.PathPatch(path, facecolor='none', lw=1)
+    ax.add_patch(patch)
+
+def desenha_ponto_inicial(ax, solucao):
+    ax.scatter(solucao[0][0], solucao[0][1], c='red')
+
+def desenha_solucao(solucao: CaminhoDoCaixeiro, title: str, file_path: str):
+    fig, ax = plt.subplots()
+    desenha_pontos(ax, solucao)
+    desenha_ponto_inicial(ax, solucao)
+    desenha_caminho(ax, solucao)
+    ax.set_title(f'{title} - Custo: {obtem_custo(solucao)}')
+    fig.savefig(file_path)
